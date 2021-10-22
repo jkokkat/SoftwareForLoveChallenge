55fb36259cdf1a6a403d3442a027a8de673d72df
Jasmine Kokkat <jazzimkokkat@gmail.com>
2021-10-21 23:41:42 -0400

    modified file structure

---
+++ .cache/_this_is_virtual_fs_path_/$virtual/sync-requires.js
@@ -0,0 +1,15 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/jasminekokkat/Downloads/gatsby-starter-default/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/jasminekokkat/Downloads/gatsby-starter-default/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/jasminekokkat/Downloads/gatsby-starter-default/src/pages/index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("/Users/jasminekokkat/Downloads/gatsby-starter-default/src/pages/page-2.js")),
+  "component---src-pages-using-ssr-js": preferDefault(require("/Users/jasminekokkat/Downloads/gatsby-starter-default/src/pages/using-ssr.js")),
+  "component---src-pages-using-typescript-tsx": preferDefault(require("/Users/jasminekokkat/Downloads/gatsby-starter-default/src/pages/using-typescript.tsx")),
+  "component---src-templates-using-dsg-js": preferDefault(require("/Users/jasminekokkat/Downloads/gatsby-starter-default/src/templates/using-dsg.js"))
+}
+
